2c662f2e1fdfd766651a026d1fa2c08ef55c30a5
Janne <janne.rissanen97@gmail.com>
2020-10-02 14:28:11 +0300

    Update KPS.java
    switch tehty

--- src/main/java/KPS.java
+++ src/main/java/KPS.java
@@ -16,10 +16,37 @@ public class KPS {
      * @.pre
      * @.post
      */
-    public static int pelaaKierros() {
+    public static int pelaaKierros(String pelaaja1, String pelaaja2) {
         //* Metodi vertaa pelaajien syöttämiä sanoja, ja selvittää, kumpi pelaajista voitti
         //* Metodi palauttaa voittaneen pelaajan numeron (1 tai 2)
-
+        switch (pelaaja1){
+            case "kivi":
+                if (pelaaja2.equals("kivi")) {
+                    return 0;
+                } else if(pelaaja2.equals("paperi")){
+                    return 2;
+                }else{
+                    return 1;
+                }
+            case "paperi":
+                if (pelaaja2.equals("paperi")) {
+                    return 0;
+                } else if(pelaaja2.equals("sakset")){
+                    return 2;
+                }else{
+                    return 1;
+                }
+            case "sakset":
+                if (pelaaja2.equals("sakset")) {
+                    return 0;
+                } else if(pelaaja2.equals("kivi")){
+                    return 2;
+                }else{
+                    return 1;
+                }
+            default:
+                System.out.println("Syötä 'kivi', 'paperi' tai 'sakset'.");
+        }
     }
 
 
