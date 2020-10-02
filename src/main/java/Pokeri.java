f38782614d9328c5b766ddca31a43638676609de
Janne <janne.rissanen97@gmail.com>
2020-10-02 16:40:24 +0300

    tehtävä 2

--- src/main/java/Pokeri.java
+++ src/main/java/Pokeri.java
@@ -29,10 +29,19 @@ public class Pokeri {
      * Palauttaa pelaajan valitseman kortin maan ja arvon (=kortin)
      * @.pre maa == "ruutu" || "hertta || "pata" || "risti"
      *          && 0 < arvo < 14
+     *          && kortti != käsi[i]
      * @.post true
      */
     public static String määritäKortti (String maa, String arvo){
         //* Metodi muuttaa pelaajan antamat syötteet yhdeksi String-tyyppiseksi kortiksi
+        //* Tarkistetaan että pelaajan antama syöte on kelvollinen.
+        if(maa != "ruutu" || maa != "risti" || maa != "pata" || maa!= "hertta") {
+            System.out.println("Syötä validi maa.");
+        }
+        int arvoint = Integer.parseInt(arvo);
+        if (0 < arvoint  && arvoint < 14) {
+            System.out.println("Anna arvo välillä 1-13.");
+        }
         String card = maa + " " + arvo;
         System.out.println("Annettu kortti on " + card );
         return card;
@@ -41,12 +50,33 @@ public class Pokeri {
     /**
      * Palauttaa tiedon, onko käsikorteissa kahta saman arvoista korttia (=paria)
      * @.pre käsi.length() == 5
-     * @.post
+     * @.post true || false
      */
     public static boolean sisältääParin(ArrayList<String> käsi) {
         //* Metodi käy läpi käsikortit, ja tarkistaa, onko korteissa kahta saman arvoista korttia
 
+        ArrayList<String[]> kortit = new ArrayList<>();
+
+        //* Erotetaan korteista arvot
+
+        for (int i = 0; i < 5; i++) {
+            String indeksi = käsi.get(i);
+            kortit.add(indeksi.split(" "));
+            for (int j = 0; j < kortit.size(); j++) {
+                kortit.remove(kortit.size() - 2);
+            }
+
+        }
+
+        //* Verrataan korttien arvoja toisiinsa parien löytämiseksi
+
+        for (int i = 0; i < kortit.size(); i++) {
+            for (int j = kortit.size() - 1; j >= 0; j--) {
+                if (kortit.get(i) == kortit.get(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-
-
 }
