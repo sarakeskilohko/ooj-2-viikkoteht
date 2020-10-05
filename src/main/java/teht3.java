a310065afc7004d0fafc0d80ba61ffb96e605399
sarakeskilohko <63452540+sarakeskilohko@users.noreply.github.com>
2020-10-05 13:35:48 +0300

    tehtävä 3 tehtävänanto

---
+++ src/main/java/teht3.java
@@ -0,0 +1,13 @@
+public class teht3 {
+
+
+    //* Tehtävä: määrittele seuraavien esitystapa:
+    //
+    // 1. Tiedonsiirron nopeus
+    // 2. Murtoluku
+    // 3. (Suomalainen) henkilön nimi
+    // 4. (Suomalainen) puhelinnumero
+    // 5. Henkilö sukututkimusohjelmassa (yksinkertaisimmillaan, kun halutaan vain selvittää sukulaisuussuhteita)
+
+
+}
